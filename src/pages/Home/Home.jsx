679c258700a4a0b8439d27d43d70ef9f64ffc06b
MykolaLukianovych <lukjanovych@gmail.com>
2025-02-18 15:51:54 +0200

    add trendMovies list

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,20 +1,29 @@
+import { useEffect, useState } from "react";
+import { TrendingMovies } from "../../services/api";
 import s from "./Home.module.css"
+import { Link } from "react-router-dom";
 
 const Home = () => {
+ 
+
+  const [trend, setTrend] = useState([]);
+  useEffect(() => {
+    const getMovies = async () => {
+      const movies = await TrendingMovies();
+      setTrend(movies)
+    }
+    getMovies();
+  })
+  
   return (
       <div className={s.homePage}>
           <h1>Trending today</h1>
-          <ul>
-              <li>any movies</li>
-              <li>any movies</li>
-              <li>any movies</li>
-              <li>any movies</li>
-              <li>any movies</li>
-              <li>any movies</li>
-              <li>any movies</li>
-              <li>any movies</li>
-              <li>any movies</li>
-              <li>any movies</li>
+      <ul className={s.trendList}>
+        {trend.map(item => <li key={item.id}>
+          <Link to={item.id.toString()}>
+            {item.original_title}
+          </Link>
+        </li>)}
           </ul>
     </div>
   )
--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,15 @@
 import axios from "axios";
 
+const options = {
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2NzYwYWZkMjNkYTU0MjFlZGQ2ODZmNmI5ZWI3ZTM0ZiIsIm5iZiI6MTczOTg3OTAwMS40NDMsInN1YiI6IjY3YjQ3MjU5NjExM2M0OTNkYzZlMDRiZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.mIYM6YRH9eHanfdCXuv7VhlnRFWrsh3hEhuXIIF3B1I'
+  }
+};
 
-axios.defaults.baseURL = "";
+
+
+export const TrendingMovies = async () => {
+    const { data } = await axios.get('https://api.themoviedb.org/3/trending/movie/day?language=en-US', options);
+    return data.results
+}
