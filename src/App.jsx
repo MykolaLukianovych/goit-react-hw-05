6ab20894cf82dc4aa4b64eb438b0df2a9a5dcf06
MykolaLukianovych <lukjanovych@gmail.com>
2025-02-18 13:26:52 +0200

    NotFound

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom"
 import Header from "./components/header/Heeder"
 import Home from "./pages/Home/Home"
 import Movies from "./pages/Movies/Movies"
+import NotFound from "./pages/NotFound/NotFound"
 
 
 const App = () => {
@@ -11,9 +12,10 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/movies" element={<Movies />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
-  )
-}
+  );
+};
 
 export default App
---
+++ src/pages/NotFound/NotFound.jsx
@@ -0,0 +1,11 @@
+import s from "./NotFound.module.css"
+
+const NotFound = () => {
+  return (
+      <div className={s.NotFound}>
+          <h1>Not Found 404</h1>
+      </div>
+  )
+}
+
+export default NotFound
