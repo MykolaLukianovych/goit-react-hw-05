65965b5d2f049dcca1dad65fecc8ce2331d9763c
MykolaLukianovych <lukjanovych@gmail.com>
2025-02-19 23:33:56 +0200

    lazy

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import { Route, Routes } from "react-router-dom"
 import Header from "./components/Header/Header"
-import Home from "./pages/Home/Home"
-import Movies from "./pages/Movies/Movies"
-import NotFound from "./pages/NotFound/NotFound"
-import MovieDetails from "./pages/MovieDetails/MovieDetails"
-import Cast from "./components/Cast/Cast"
-import Reviews from "./components/Reviews/Reviews"
+// import Home from "./pages/Home/Home"
+// import Movies from "./pages/Movies/Movies"
+// import NotFound from "./pages/NotFound/NotFound"
+// import MovieDetails from "./pages/MovieDetails/MovieDetails"
+// import Cast from "./components/Cast/Cast"
+// import Reviews from "./components/Reviews/Reviews"
+import { lazy } from "react"
+
+
+const Home = lazy(() => import("./pages/Home/Home"))
+const Movies = lazy(() => import("./pages/Movies/Movies"))
+const NotFound = lazy(() => import("./pages/NotFound/NotFound"))
+const MovieDetails = lazy(() => import("./pages/MovieDetails/MovieDetails"))
+const Cast = lazy(() => import("./components/Cast/Cast"))
+const Reviews = lazy(() => import("./components/Reviews/Reviews"))
 
 
 const App = () => {
